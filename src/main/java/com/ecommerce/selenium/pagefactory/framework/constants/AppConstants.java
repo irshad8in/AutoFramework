92c4213d2d7aac34230e483b99bcbaab05cabcd9
irshad8in <33025012+irshad8in@users.noreply.github.com>
2020-07-27 12:56:52 -0500

    Update AppConstants.java

--- src/main/java/com/ecommerce/selenium/pagefactory/framework/constants/AppConstants.java
+++ src/main/java/com/ecommerce/selenium/pagefactory/framework/constants/AppConstants.java
@@ -4,7 +4,7 @@ public interface AppConstants {
 	
 	static final String PERFECTO_URL="partners.perfectomobile.com";
 	static final String PERFECTO_USRNAME="demo@newtglobal.com";
-	static final String PERFECTO_PWD="DEMO123";
+	static final String PERFECTO_PWD="";
 	static final String PERFECTO_BROWSER="mobileOS";
 	static final String APP_URL="www.moneycontrol.com";
 
